2183cc840cd2118f1fd5e274a1804b4949daaa72
jhagstrand <john@hagstrand.com>
2017-05-19 04:33:10 +0000

    Add account/svc

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-# build account
+# build model
 
 if [ "$#" -ne 2 ]; then
 	echo Usage: ./build.sh prod compact or ./build.sh dev pretty
@@ -8,7 +8,7 @@ fi
 python compilejs.py $1 $2 >html/min.js
 
 # concatenate and compile the css files
-cat html/normalize.css/normalize.css html/minimal/minimal.css html/minimal/theme/mahagony.css html/icon/icon.css html/css/account.css >html/min.css
+cat html/minimal/normaleyes.css html/minimal/minimal.css html/minimal/theme/mahagony.css html/icon/icon.css html/css/model.css >html/min.css
 java -jar /home/jhagstrand/bin/yuicompressor/yuicompressor-2.4.2.jar html/min.css -o html/min.css --charset utf-8
 
 # prepare index.php for production use
---
+++ html/account/svc.php
@@ -0,0 +1,4 @@
+<?php
+include("../../php/account/svchub.php");
+svchub();
+?>
