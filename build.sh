778ef0c73e6272adc156e46101747d4244b8c9a7
voyccom <john@hagstrand.com>
2019-09-12 02:53:57 -0700

    fix build

--- build.sh
+++ build.sh
@@ -5,13 +5,18 @@ if [ "$#" -ne 2 ]; then
 fi
 
 # compile the js files with google closure compiler
+echo "compile js with google closure compiler"
 python compilejs.py $1 $2 >html/min.js
 
 # concatenate and compile the css files
-cat html/minimal/normaleyes.css html/minimal/minimal.css html/minimal/theme/mahagony.css html/icon/icon.css html/css/model.css >html/min.css
-java -jar /home/jhagstrand/bin/yuicompressor/yuicompressor-2.4.2.jar html/min.css -o html/min.css --charset utf-8
+echo "minify the css files"
+cat html/minimal/normaleyes.css html/minimal/minimal.css html/minimal/theme/mahagony.css html/icon/icon.css html/css/model.css | 
+    sed 's/+/%2b/g'  >html/min.css
+wget --post-data="input=`cat html/min.css`" --output-document=html/min.css https://cssminifier.com/raw
 
 # prepare index.php for production use
+echo "fix index.php"
 cp html/index.html html/index.php
 sed -i -e 's/<!--<remove>//g' html/index.php
 sed -i -e 's/<remove>-->//g' html/index.php
+echo "complete"
